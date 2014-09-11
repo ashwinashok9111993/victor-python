53d042618f93ef5d020ec82afc1957dea60edea7
ashwin <ashwinashok9111993@gmail.com>
2014-09-12 00:15:06 +0530

    Create advance.py

---
+++ advance.py
@@ -0,0 +1,30 @@
+#advanced python
+
+#multiple assignment
+x,y,z=2,3,4
+print x,y,z
+
+#swapping
+x,y,z=z,y,x
+print x,y,z
+
+#sequence types
+# tuple
+tu= (23,"deepak",3+5j,2.45)
+print tu
+
+#list
+l1=[23,"deepak",3+5j,2.45]
+print l1
+print l1[3]
+
+l1.append("list can be appended")
+print l1
+
+# copy same elements to list
+l1.append(l1)
+print l1
+print l1[5]
+
+#eof
+

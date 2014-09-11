ad2a61673df7d856985991ac7efe47947870a58a
ashwin <ashwinashok9111993@gmail.com>
2014-09-12 00:13:23 +0530

    updates

---
+++ test.py
@@ -0,0 +1,38 @@
+# test file
+print "Hello World"
+
+# data types
+a=1
+print a
+b=2.1
+print b
+c_real=3
+c_img=4
+print c_real,"+",c_img,"i"
+
+# ashwin is an idiot
+print 3+4j
+
+#strings
+s1='hi'
+s2="hi"
+s3="""hi"""
+print s1,s2,s3
+
+
+#string manupilation
+
+s4=s1+" how"
+print s4
+
+#arithemetic
+print a+b
+print 2**5
+
+#methods
+print len(s4)
+
+
+
+
+
